8338e150470029148de387b9ed4c98ec5536e2bd
Shalanzia <salaamxdd@gmail.com>
2024-06-28 16:35:11 -0600

    updateUser

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+        name: string;
+        password: string;
+        firstName: string;
+        phone: string;
+        email: string;
+        admin?: boolean;
+        userPhoto?: string;
+    
+}

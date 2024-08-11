a40517df31532e87a78e6767948b77a55df2b5c5
Shalanzia <salaamxdd@gmail.com>
2024-08-10 20:57:43 -0600

    stoppoint Docs

--- src/modules/stoppoints/entities/stoppoint.entity.ts
+++ src/modules/stoppoints/entities/stoppoint.entity.ts
@@ -1,1 +1,12 @@
-export class Stoppoint {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class StopPoint {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: 'Punto de parada 1' })
+  name: string;
+
+  @ApiProperty({ example: 1 })
+  excursionId: number;
+}

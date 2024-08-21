2b0158c8affd15ee56d449a53d97113bb2e12dc0
Martin <martinfsgang@gmail.com>
2024-08-21 16:05:06 -0600

    Documentacióm api del módulo activity

--- src/modules/activities/entities/activity.entity.ts
+++ src/modules/activities/entities/activity.entity.ts
@@ -1,1 +1,13 @@
-export class Activity {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class Activity {
+        @ApiProperty({ example: 'Nombre de la actividad' })
+        name: string;
+      
+        @ApiProperty({ example: 1 })
+        numActivity: number
+      
+        @ApiProperty({ example: 'Descripción de la actividad' })
+        description: string
+    }
+
